6948baeae0db085b2f6378f3596a78c4895e8dbe
unknown <arthurluiz.developer@gmail.com>
2026-06-04 21:50:15 +0100

    New card hover animation added.

---
+++ Assets/Scripts/Cards/CardUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Vector2 pivotWhenIdle = new(0, 0.5f);
+    [SerializeField] private Vector2 pivotWhenMouseHover = new(0, 0);
+
+    private RectTransform rectTransform;
+    private Vector2 targetPivot;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        rectTransform.pivot = Vector2.Lerp(rectTransform.pivot, targetPivot, 6f);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        targetPivot = pivotWhenMouseHover;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        targetPivot = pivotWhenIdle;
+    }
+}
